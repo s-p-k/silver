e418ca450ac413bb958821dc5e18cc580b46a7ed
s-p-k <mr.dwts@gmail.com>
2014-05-02 01:27:45 +0300

    count number of bookmarks that match a specified tag

--- silver.c
+++ silver.c
@@ -11,10 +11,12 @@ void matches(char *tag);
 
 void searchTag(const char *tag);
 
+int countMarks(char *fname, char *str);
+
 void
 usage(const char *arg)
 {
-	printf("Usage: %s [-h] [-s tag]\n", arg);
+	printf("Usage: %s [-h] [-c tag]\n", arg);
 
 }
 
@@ -22,8 +24,8 @@ int
 main(int argc, char *argv[])
 {
 
-	int opt;
-	int hflag = 0, nflag = 0, sflag = 0;
+	int opt, result;
+	int hflag = 0, nflag = 0, cflag = 0;
 	char *tag;
 
 	if (argc == 1) {
@@ -31,7 +33,7 @@ main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 	
-	while ((opt = getopt(argc, argv, "sn:h")) != -1) {
+	while ((opt = getopt(argc, argv, "cn:h")) != -1) {
 		switch (opt) {
 		case 'h':
 			hflag = 1;
@@ -40,8 +42,8 @@ main(int argc, char *argv[])
 			nflag = 1;
 			tag = optarg;
 			break;
-		case 's':
-			sflag = 1;
+		case 'c':
+			cflag = 1;
 			tag = optarg;
 			break;
 		default:
@@ -53,8 +55,11 @@ main(int argc, char *argv[])
 		usage(argv[0]);
 	if (nflag)
 		matches(tag);
-	if (sflag)
-		searchTag(tag);
+	if (cflag) {
+		result = countMarks(BOOKMARKS, argv[2]);
+		if(!result)
+			return(EXIT_FAILURE);
+	}
 
 	return EXIT_SUCCESS;
 }
@@ -65,19 +70,45 @@ matches(char *tag)
 	printf("not yet implemented %s\n", tag);
 }
 
-void
-searchTag(const char *tag)
-{
-	char line[LINE_MAX];
-	FILE *fl;
+/* void */
+/* searchTag(const char *tag) */
+/* { */
+/* 	char line[LINE_MAX]; */
+/* 	FILE *fl; */
 	
-	fl = fopen(BOOKMARKS, "r");
-	if (!fl)
-		warn("%p", fl);
-	while(fgets(line, sizeof(line), fl) != NULL) {
-		if (strstr(line, tag))
-			printf("%s", line);
+/* 	fl = fopen(BOOKMARKS, "r"); */
+/* 	if(!fl) */
+/* 		warn("%p", fl); */
+/* 	while(fgets(line, sizeof(line), fl) != NULL) { */
+/* 		if (strstr(line, tag)) */
+/* 			printf("%s", line); */
+/* 	} */
+/* 	fclose(fl); */
+/* 	return; */
+/* } */
+
+
+/* count bookmarks that are tagged with "tag" */
+
+int
+countMarks(char *fname, char *str)
+{
+	FILE *fp;
+	int nl = 1;
+	int found = 0;
+	char temp[512];
+	
+	if((fp = fopen(fname, "r")) == NULL)
+		return(EXIT_FAILURE);
+
+	while(fgets(temp, 512, fp) != NULL) {
+		if((strstr(temp, str)) != NULL)
+			found++;
+		nl++;
 	}
-	fclose(fl);
-	return;
+
+	printf("%d\n", found);
+	fclose(fp);
+
+	return(EXIT_SUCCESS);
 }
